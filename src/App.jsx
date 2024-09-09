6526b0c673637bd18d07c3bb455d98a4ba64ebf8
Edward Zhang <junz23@uci.edu>
2024-09-09 07:25:49 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+export default function Header () {
+    return (
+        <>
+            <nav>
+                
+            </nav>
+        </>
+    )
+}
